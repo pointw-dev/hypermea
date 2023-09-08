d68c252b8587b7b1ef885f646d9ee51b83ca7e00
Michael Ottoson <michael@pointw.com>
2023-09-08 12:20:04 -0400

    handle 'allow_unknown' in RFC6861

--- src/hypermea/skel/api/affordances/rfc6861/_common.py
+++ src/hypermea/skel/api/affordances/rfc6861/_common.py
@@ -6,7 +6,7 @@ def generate_hal_forms_template(method, schema, self_ref, resource=None):
         if field.startswith('_'):
             continue
 
-        field_type = field_info.get('type')
+        field_type = field_info.get('type', 'string')
         hal_field = {
             'name': field,
             'prompt': f'Enter {field}',
@@ -15,40 +15,43 @@ def generate_hal_forms_template(method, schema, self_ref, resource=None):
             'value': resource[field] if resource else '',
         }
 
-        if 'allowed' in field_info:
-            hal_field['options'] = {
-                'inline': field_info['allowed'],
-                'maxItems': 1
-            }
-            if 'default' in field_info:
-                hal_field['options']['selectedValues'] = [field_info['default']]
-            if field in resource:
-                hal_field['options']['selectedValues'] = [resource[field]]
+        if field_info.get('allow_unknown'):
+            hal_field['hyAnyObject'] = True
+        else:
+            if 'allowed' in field_info:
+                hal_field['options'] = {
+                    'inline': field_info['allowed'],
+                    'maxItems': 1
+                }
+                if 'default' in field_info:
+                    hal_field['options']['selectedValues'] = [field_info['default']]
+                if field in resource:
+                    hal_field['options']['selectedValues'] = [resource[field]]
 
-        if 'min' in field_info:
-            hal_field['min'] = field_info['min']
+            if 'min' in field_info:
+                hal_field['min'] = field_info['min']
 
-        if 'max' in field_info:
-            hal_field['max'] = field_info['max']
+            if 'max' in field_info:
+                hal_field['max'] = field_info['max']
 
-        if 'maxlength' in field_info:
-            hal_field['maxLength'] = field_info['maxlength']
+            if 'maxlength' in field_info:
+                hal_field['maxLength'] = field_info['maxlength']
 
-        if 'minlength' in field_info:
-            hal_field['minLength'] = field_info['minlength']
+            if 'minlength' in field_info:
+                hal_field['minLength'] = field_info['minlength']
 
-        if 'regex' in field_info:
-            hal_field['pattern'] = field_info['regex']
+            if 'regex' in field_info:
+                hal_field['pattern'] = field_info['regex']
 
-        if field_type == 'datetime':
-            hal_field['format'] = 'date-time'
+            if field_type == 'datetime':
+                hal_field['format'] = 'date-time'
 
         properties.append(hal_field)
 
     template['_templates'] = {
         'default': {
             'method': method,
-            'title': 'Soon is coming',
+            # 'title': 'Soon is coming',
             'contentType': 'application/json',
             'properties': properties,
         }
