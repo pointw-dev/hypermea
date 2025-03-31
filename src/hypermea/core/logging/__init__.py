e1e22e29fca63665635dcf2b659ee86107c3d85f
Michael Ottoson <michael@pointw.com>
2025-03-30 22:55:56 -0400

    Tidying, plus #113

--- src/hypermea/core/logging/__init__.py
+++ src/hypermea/core/logging/__init__.py
@@ -4,6 +4,8 @@ from .trace.trace_level import TRACE_LEVEL
 from .trace.decorators import trace
 from configuration import SETTINGS
 
+from ..utils import get_resource_rel
+
 TRUNCATED = '...\n  [Body truncated. Set logging to TRACE for full body, or increase HY_LOG_MAX_BODY_SIZE.]'
 
 
@@ -43,6 +45,7 @@ def log_request(log, resource, request, lookup):
 @trace
 def log_response(log, resource, request, payload):
     requested = resource if resource else 'home'
+    rel = get_resource_rel(resource)
 
     current_console_level = _get_console_handler_level()
     if current_console_level is None or current_console_level >= logging.INFO:
@@ -58,9 +61,11 @@ def log_response(log, resource, request, payload):
     max_body_size = SETTINGS['HY_LOG_MAX_BODY_SIZE']
     if current_console_level >= 10 and len(response_body) > max_body_size:
         response_body = response_body[:max_body_size] + TRUNCATED
-        if payload.is_json and '_items' in payload.json:
-            num_items = len(payload.json["_items"])
-            response_body += f'\n  Collection has {num_items} {"item" if num_items == 1 else "items"}'
+        if payload.is_json:
+            collection = payload.json.get('_embedded', {}).get(rel, None)
+            if collection and isinstance(collection, list):
+                num_items = len(collection)
+                response_body += f'\n  Collection has {num_items} {"item" if num_items == 1 else "items"}'
 
     log.debug(f'Response sent for {request} {requested} [{payload.status_code}]\n'
               f'-response.headers:\n  {response_headers}\n'
